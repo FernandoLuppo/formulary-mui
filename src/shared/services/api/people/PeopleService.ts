ebdb092d9d9c2560db70799d171d6d652d74b6a6
Fernando José Alves Luppo <fernandoluppo2@gmail.com>
2023-03-16 19:39:36 -0300

    feat: Terminando configurações do PeopleService

--- src/shared/services/api/people/PeopleService.ts
+++ src/shared/services/api/people/PeopleService.ts
@@ -1,51 +1,65 @@
-import { Environment } from "../../../environment"
-import type { IPeopleWithTotalCount } from "../../../types"
 import { Api } from "../axios-config"
+import { Environment } from "../../../environment"
+import type { IPeopleDetails, IPeopleWithTotalCount } from "../../../types"
 
-const getAll = async (page = 1, filter = ""): Promise<IPeopleWithTotalCount | Error> => {
+const getAll = async (
+    page = 1,
+    filter = ""
+): Promise<IPeopleWithTotalCount | Error> => {
     try {
         const urlRelative = `/people?_page=${page}&_limit=${Environment.LINE_LIMIT}&fullName_like=${filter}`
         const { data, headers } = await Api.get(urlRelative)
 
-        if (data) {
+        if (data !== null) {
             return {
                 data,
-                totalCount: Number(headers["x-total-count"]) || Environment.LINE_LIMIT
+                totalCount:
+                    Number(headers["x-total-count"]) | Environment.LINE_LIMIT
             }
         }
 
         return new Error("Error to try list the records.")
-
-    }  catch(error) {
-        return new Error( (error as {message: string}).message || "Error to try list the records.")
+    } catch (error) {
+        return new Error("Error to try list the records.")
     }
 }
-const getById = async (): Promise<any> => {
+const getById = async (id: number): Promise<IPeopleDetails | Error> => {
     try {
-        const { data } = await Api.
-    } catch(error) {
+        const { data } = await Api.get(`/people/${id}`)
+        if (data !== null) return data
 
+        return new Error("Error to try list the records.")
+    } catch (error) {
+        return new Error("Error to try list the records.")
     }
 }
-const create = async (): Promise<any> => {
+const create = async (
+    user: Omit<IPeopleDetails, "id">
+): Promise<number | Error> => {
     try {
-        const { data } = await Api.
-    } catch(error) {
+        const { data } = await Api.post<IPeopleDetails>("/people", user)
+        if (data !== null) return data.id
 
+        return new Error("Error to try create the records.")
+    } catch (error) {
+        return new Error("Error to try create the records.")
     }
 }
-const updateById = async (): Promise<any> => {
+const updateById = async (
+    id: number,
+    user: IPeopleDetails
+): Promise<undefined | Error> => {
     try {
-        const { data } = await Api.
-    } catch(error) {
-
+        await Api.put(`/people/${id}`, user)
+    } catch (error) {
+        return new Error("Error to try update the records.")
     }
 }
-const deleteById = async (): Promise<any> => {
+const deleteById = async (id: number): Promise<undefined | Error> => {
     try {
-        const { data } = await Api.
-    } catch(error) {
-
+        await Api.delete(`/people/${id}`)
+    } catch (error) {
+        return new Error("Error to try delete the records.")
     }
 }
 
